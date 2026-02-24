1b9f6444dd18cd1baf6e5735fafdd36b5d7821af
Alex1808 <sadam6752@gmail.com>
2026-02-24 14:12:50 +0100

    v1.0.25: index.html + jsonConfig.js для selectID

---
+++ admin/jsonConfig.js
@@ -0,0 +1,10 @@
+// This file is auto-generated
+window.adapterSettings = {
+    url: 'jsonConfig.json',
+    onLoad: function (settings, onChange) {
+        load(settings, onChange);
+    },
+    onSave: function (callback) {
+        save(callback);
+    }
+};
