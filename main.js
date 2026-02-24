8bc572abd3f3f34a9b559632ec975dbb67ff860b
Alex1808 <sadam6752@gmail.com>
2026-02-24 18:45:35 +0100

    v1.0.52: onMessage bind entfernt

--- main.js
+++ main.js
@@ -83,9 +83,6 @@ class PvNotifications extends utils.Adapter {
 
         this.log.info('PV Notifications Adapter gestartet');
 
-        // Message-Handler registrieren
-        this.on('message', this.onMessage.bind(this));
-
         // Konfiguration loggen
         this.log.info(`Konfiguration: Voll=${this.config.thresholdFull}%, Leer=${this.config.thresholdEmpty}%, Intermediate=[${this.config.intermediateSteps}]`);
 
